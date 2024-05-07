77ca6c9864f7897dbc4509a1c474a90c742d0f76
sunnyswag <hhq126152@gmail.com>
2024-05-07 09:52:14 +0800

    resolve build script

--- packages/plugin-ui/src/PluginUI.tsx
+++ packages/plugin-ui/src/PluginUI.tsx
@@ -1,68 +1,45 @@
-import React, { useState } from 'react';
-import type { RadioChangeEvent } from 'antd';
-import { Radio } from 'antd';
+import React from 'react';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Flex, Radio, Button } from 'antd';
 
-const plainOptions = ['Apple', 'Pear', 'Orange'];
-const options = [
-  { label: 'Apple', value: 'Apple' },
-  { label: 'Pear', value: 'Pear' },
-  { label: 'Orange', value: 'Orange', title: 'Orange' },
-];
-const optionsWithDisabled = [
-  { label: 'Apple', value: 'Apple' },
-  { label: 'Pear', value: 'Pear' },
-  { label: 'Orange', value: 'Orange', disabled: true },
-];
+export const MainUI: React.FC = () => (
+  <Flex vertical>
+    <ProcessTypeView />
+    <CodePreview code='' />
+    <CopyToClipBoard />
+  </Flex>
+);
 
-const App: React.FC = () => {
-  const [value1, setValue1] = useState('Apple');
-  const [value2, setValue2] = useState('Apple');
-  const [value3, setValue3] = useState('Apple');
-  const [value4, setValue4] = useState('Apple');
-
-  const onChange1 = ({ target: { value } }: RadioChangeEvent) => {
-    console.log('radio1 checked', value);
-    setValue1(value);
-  };
-
-  const onChange2 = ({ target: { value } }: RadioChangeEvent) => {
-    console.log('radio2 checked', value);
-    setValue2(value);
-  };
-
-  const onChange3 = ({ target: { value } }: RadioChangeEvent) => {
-    console.log('radio3 checked', value);
-    setValue3(value);
-  };
-
-  const onChange4 = ({ target: { value } }: RadioChangeEvent) => {
-    console.log('radio4 checked', value);
-    setValue4(value);
-  };
-
-  return (
-    <>
-      <Radio.Group options={plainOptions} onChange={onChange1} value={value1} />
-      <br />
-      <Radio.Group options={optionsWithDisabled} onChange={onChange2} value={value2} />
-      <br />
-      <br />
-      <Radio.Group options={options} onChange={onChange3} value={value3} optionType="button" />
-      <br />
-      <br />
-      <Radio.Group
-        options={optionsWithDisabled}
-        onChange={onChange4}
-        value={value4}
-        optionType="button"
-        buttonStyle="solid"
-      />
-    </>
-  );
-};
-
-export default App;
-
-export const PluginUI = () => {
-    <Radio.Group
+enum ProcessType {
+  XmlResource,
+  Compose
 }
+
+const ProcessTypeView: React.FC = () => (
+  <Radio.Group defaultValue={ProcessType.XmlResource} buttonStyle="solid">
+    <Radio.Button value={ProcessType.XmlResource}>Xml Resource</Radio.Button>
+    <Radio.Button value={ProcessType.Compose}>Compose</Radio.Button>
+  </Radio.Group>
+);
+
+const CodePreview: React.FC<{ code: string }> = ({ code }) => (
+  <SyntaxHighlighter
+    language="xml"
+    customStyle={{
+      fontSize: 12,
+      borderRadius: 8,
+      marginTop: 0,
+      marginBottom: 0,
+      backgroundColor: "#1B1B1B",
+      transitionProperty: "all",
+      transitionTimingFunction: "ease",
+      transitionDuration: "0.2s",
+    }}
+  >
+    {code}
+  </SyntaxHighlighter>
+);
+
+const CopyToClipBoard: React.FC = () => (
+  <Button type="primary">Copy To Clipboard</Button>
+);
