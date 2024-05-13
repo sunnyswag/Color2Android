e552092917312b58081c449702b8fc4850294b64
sunnyswag <hhq126152@gmail.com>
2024-05-13 10:15:19 +0800

    update

--- app/debug/next.config.mjs
+++ app/debug/next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["ui", "plugin-ui"],
+  publicRuntimeConfig: {
+    site: {
+      name: "Next.js + Tailwind CSS template",
+      url:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000"
+          : "https://earvinpiamonte-nextjs-tailwindcss-template.vercel.app",
+      title: "Next.js + Tailwind CSS template",
+      description: "Next.js + Tailwind CSS template",
+      socialPreview: "/images/preview.png",
+    },
+  },
+  swcMinify: true,
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
