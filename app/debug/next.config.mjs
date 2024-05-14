0225b8900f13f5dfde15f9146dbecc0ec647c5af
sunnyswag <hhq126152@gmail.com>
2024-05-14 09:35:19 +0800

    fix: next.js config error

--- app/debug/next.config.mjs
+++ app/debug/next.config.mjs
@@ -1,32 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["ui", "plugin-ui"],
-  publicRuntimeConfig: {
-    site: {
-      name: "Next.js + Tailwind CSS template",
-      url:
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:3000"
-          : "https://earvinpiamonte-nextjs-tailwindcss-template.vercel.app",
-      title: "Next.js + Tailwind CSS template",
-      description: "Next.js + Tailwind CSS template",
-      socialPreview: "/images/preview.png",
-    },
-  },
-  swcMinify: true,
-  i18n: {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  }
+  transpilePackages: ["plugin-ui"],
 };
 
 export default nextConfig;
