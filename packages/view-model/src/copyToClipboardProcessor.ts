1ed2954444726b3109b7f605fe34915a0374d66e
sunnyswag <hhq126152@gmail.com>
2024-05-20 09:43:02 +0800

    process storage get and set

--- packages/view-model/src/copyToClipboardProcessor.ts
+++ packages/view-model/src/copyToClipboardProcessor.ts
@@ -1,10 +1,13 @@
-import { PluginUIEvent, UIEventMsgType } from "model";
+import { PluginUIEvent, UIEventMsgType, curUIStateData } from "model";
 import { IEventProcessor } from "./iEventProcessor";
 
 export class CopyToClipboardProcessor implements IEventProcessor {
   messageType = UIEventMsgType.CopyToClipboard;
 
   async process(_event: PluginUIEvent) {
+    await figma.clientStorage.setAsync(UIStateKey, curUIStateData)
     return false;
   }
 }
+
+export const UIStateKey = "UIStateKey"
--- packages/view-model/src/initPluginProcessor.ts
+++ packages/view-model/src/initPluginProcessor.ts
@@ -1,10 +1,11 @@
-import { PluginUIEvent, UIEventMsgType } from "model";
+import { PluginUIEvent, UIEventMsgType, updateCurUIStateData } from "model";
 import { IEventProcessor } from "./iEventProcessor";
+import { UIStateKey } from "./copyToClipboardProcessor";
 
 export class InitPluginProcessor implements IEventProcessor {
   messageType = UIEventMsgType.InitPlugin;
 
   async process(_event: PluginUIEvent) {
-    
+    updateCurUIStateData(await figma.clientStorage.getAsync(UIStateKey))
   }
 }
